69d11dae062e4e52bddad534fa064a56ab9516fe
Spencer Gilbert <spencer.gilbert@datadoghq.com>
2022-03-08 14:30:57 -0500

    chore: Release vector-0.7.0-rc.0 (#168)
    * chore(deps): bump actions/checkout from 2 to 3 (#165)
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 2 to 3.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v2...v3)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feat(vector): Add list verb for upcoming kube-rs change (#166)
    
    * feat(vector): Add list verb for upcoming kube-rs change
    
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>
    
    * version bump
    
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>
    
    * update cliff config due to new version, use long args
    
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>
    
    * generate changelog
    
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>
    
    * chore: Fix metadata for 0.7.0-rc.0 release (#167)
    
    Signed-off-by: Spencer Gilbert <spencer.gilbert@datadoghq.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- .github/release-changelog.sh
+++ .github/release-changelog.sh
@@ -6,4 +6,4 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 set -x
 
 _chart_version=$(yq eval .version "charts/vector/Chart.yaml")
-git cliff --config .github/cliff.toml -t "vector-$_chart_version" -p CHANGELOG.md -u
+git cliff --config .github/cliff.toml --tag "vector-$_chart_version" --prepend CHANGELOG.md --unreleased
