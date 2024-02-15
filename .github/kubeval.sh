84f506a54d311588ef5170435dac2bc26e389f60
Jesse Szwedko <jesse.szwedko@datadoghq.com>
2024-02-15 11:48:22 -0800

    chore(vector): Run CI on any changes
    Without this PRs that don't make changes to any charts don't pass CI.
    I think the overhead of just testing all charts is fairly minimal.
    
    Signed-off-by: Jesse Szwedko <jesse.szwedko@datadoghq.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -6,13 +6,7 @@ KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/" # Up-to-date fork of instrumenta/kubernetes-json-schema
 OS=$(uname)
 
-CHANGED_CHARTS=${CHANGED_CHARTS:-${1:-}}
-if [ -n "$CHANGED_CHARTS" ];
-then
-  CHART_DIRS=$CHANGED_CHARTS
-else
-  CHART_DIRS=$(ls -d charts/*)
-fi
+CHART_DIRS="charts/vector charts/vector-aggregator charts/vector-agent"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/v"${KUBEVAL_VERSION}"/kubeval-${OS}-amd64.tar.gz
