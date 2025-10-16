35b0073d0ec420f7e43b789502eb4fc66309a5b5
Enin <enin.kaduk@docker.com>
2025-10-16 12:17:51 +0200

    fix(ci): allow macOS contributions

--- .github/release-vector-version.sh
+++ .github/release-vector-version.sh
@@ -14,4 +14,7 @@ set -x
 _VERSION=$(curl --silent https://api.github.com/repos/vectordotdev/vector/releases/latest \
   | grep -oE "tag_name\": *\".{1,15}\"," \
   | ${SED:-sed} 's/tag_name\": *\"v//;s/\",//')
-${SED:-sed} -E -i "s/([0-9]+)\.([0-9]+)\.([0-9]+)-distroless-libc/$_VERSION-distroless-libc/" charts/vector/Chart.yaml
+
+# make MacOS sed happy
+${SED:-sed} -E -i.bak "s/([0-9]+)\.([0-9]+)\.([0-9]+)-distroless-libc/${_VERSION}-distroless-libc/" charts/vector/Chart.yaml
+rm -f "charts/vector/Chart.yaml.bak"
